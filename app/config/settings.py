d3d242933c71bfc3d81513f211bd3ae1e7ab7afb
Eise Olusegun William <Williamseisenherz@gmail.com>
2023-10-30 06:57:33 +0100

    fix

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,9 +2,9 @@ from decouple import config
 
 ACCESS_TOKEN_EXPIRES_IN = 60
 REFRESH_TOKEN_EXPIRES_IN = 20
-URI = str(config("DB_URL"))
-JWT_SECRET_KEY = str(config("JWT_SECRET_KEY"))   
-JWT_REFRESH_SECRET_KEY = str(config("JWT_SECRET_KEY"))
-ALGORITHM = str(config("JWT_ALGORITHM", default="HS256"))
-ACCESS_TOKEN_EXPIRE_MINUTES = int(config("ACCESS_TOKEN_EXPIRES_IN", default=60))
-REFRESH_TOKEN_EXPIRE_MINUTES = int(config("REFRESH_TOKEN_EXPIRES_IN", default=20))
+URI = config("DB_URL", default="mongodb://localhost:27017", cast=str)
+JWT_SECRET_KEY = config("JWT_SECRET_KEY", cast=str)   
+JWT_REFRESH_SECRET_KEY = config("JWT_SECRET_KEY", cast=str)
+ALGORITHM = config("JWT_ALGORITHM", default="HS256", cast=str)
+ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRES_IN", default=60, cast=int)
+REFRESH_TOKEN_EXPIRE_MINUTES = config("REFRESH_TOKEN_EXPIRES_IN", default=20, cast=int)
